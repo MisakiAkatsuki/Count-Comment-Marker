e9ec232d06f54750d8fad293f0587bf5f6e552b7
Misaki_Akatsuki <mikuchixip@live.jp>
2016-11-11 06:46:13 +0900

    Packaging zip 110

--- src/Count Comment Marker.js
+++ src/Count Comment Marker.js
@@ -1,22 +1,3 @@
-/*
-  Count Comment Marker
-    (C) あかつきみさき(みくちぃP)
-
-  このスクリプトについて
-    選択したレイヤーにマーカーの数+1カウントのコメントがついたマーカーを追加するスクリプト.
-    例:レイヤーのマーカーの数が3つなら「4」のコメントがついたマーカーを追加
-
-    マーカーが追加される時間に存在する場合はマーカーの数が変わらない為,コメントは元のマーカーの数と同じ数字になります.
-
-  使用方法
-    1.ファイル→スクリプト→スクリプトファイルの実行から実行.
-
-  動作環境
-    Adobe After Effects CS6以上
-
-  バージョン情報
-    2016/10/23 Ver 1.0.0 Release
-*/
 /// <reference path="C:/Users/RUI/OneDrive/lib/aftereffects.d.ts/ae.d.ts"/>
 (function () {
     var ADBE_MARKER = "ADBE Marker";
@@ -40,21 +21,18 @@
     if (!isCompActive(actComp)) {
         return 0;
     }
-    var selLayers = actComp.selectedLayers;
-    if (!isLayerSelected(selLayers)) {
-        return 0;
-    }
     app.beginUndoGroup("Count Comment Marker");
     var keyNum = 0;
     var markerObj;
-    var actMarkerTime;
-    for (var i = 0; i < selLayers.length; i++) {
-        keyNum = selLayers[i].property(ADBE_MARKER).numKeys;
+    var actMarkerTime = 0;
+    var selLayers = actComp.selectedLayers;
+    if (!isLayerSelected(selLayers)) {
+        keyNum = actComp.markerProperty.numKeys;
         if (keyNum == 0) {
             markerObj = new MarkerValue(keyNum + 1);
         }
         else {
-            actMarkerTime = selLayers[i].property(ADBE_MARKER).keyTime(selLayers[i].property(ADBE_MARKER).nearestKeyIndex(actComp.time));
+            actMarkerTime = actComp.markerProperty.keyTime(actComp.markerProperty.nearestKeyIndex(actComp.time));
             if (actMarkerTime == actComp.time) {
                 markerObj = new MarkerValue(keyNum);
             }
@@ -62,7 +40,25 @@
                 markerObj = new MarkerValue(keyNum + 1);
             }
         }
-        selLayers[i].property(ADBE_MARKER).setValueAtTime(actComp.time, markerObj);
+        actComp.markerProperty.setValueAtTime(actComp.time, markerObj);
+        return 0;
+    }
+    for (var _i = 0, selLayers_1 = selLayers; _i < selLayers_1.length; _i++) {
+        var layer = selLayers_1[_i];
+        keyNum = layer.property(ADBE_MARKER).numKeys;
+        if (keyNum == 0) {
+            markerObj = new MarkerValue(keyNum + 1);
+        }
+        else {
+            actMarkerTime = layer.property(ADBE_MARKER).keyTime(layer.property(ADBE_MARKER).nearestKeyIndex(actComp.time));
+            if (actMarkerTime == actComp.time) {
+                markerObj = new MarkerValue(keyNum);
+            }
+            else {
+                markerObj = new MarkerValue(keyNum + 1);
+            }
+        }
+        layer.property(ADBE_MARKER).setValueAtTime(actComp.time, markerObj);
     }
     app.endUndoGroup();
 }).call(this);
--- src/Count Comment Marker.ts
+++ src/Count Comment Marker.ts
@@ -1,29 +1,9 @@
-/*
-  Count Comment Marker
-    (C) あかつきみさき(みくちぃP)
-
-  このスクリプトについて
-    選択したレイヤーにマーカーの数+1カウントのコメントがついたマーカーを追加するスクリプト.
-    例:レイヤーのマーカーの数が3つなら「4」のコメントがついたマーカーを追加
-
-    マーカーが追加される時間に存在する場合はマーカーの数が変わらない為,コメントは元のマーカーの数と同じ数字になります.
-
-  使用方法
-    1.ファイル→スクリプト→スクリプトファイルの実行から実行.
-
-  動作環境
-    Adobe After Effects CS6以上
-
-  バージョン情報
-    2016/10/23 Ver 1.0.0 Release
-*/
-
 /// <reference path="C:/Users/RUI/OneDrive/lib/aftereffects.d.ts/ae.d.ts"/>
 
-(function() {
-	const ADBE_MARKER:string = "ADBE Marker";
+(function () {
+  const ADBE_MARKER: string = "ADBE Marker";
 
-  const isCompActive = function(comp:CompItem) {
+  const isCompActive = function (comp: CompItem) {
     if (!(comp && comp instanceof CompItem)) {
       return false;
     } else {
@@ -31,7 +11,7 @@
     }
   }
 
-  const isLayerSelected = function(layers:Layer[]) {
+  const isLayerSelected = function (layers: Layer[]) {
     if (layers.length === 0) {
       return false;
     } else {
@@ -39,38 +19,53 @@
     }
   }
 
-	const actComp:CompItem = <CompItem>app.project.activeItem;
+  const actComp: CompItem = <CompItem>app.project.activeItem;
   if (!isCompActive(actComp)) {
     return 0;
   }
 
-	const selLayers:Layer[] = <Layer[]>actComp.selectedLayers;
+  app.beginUndoGroup("Count Comment Marker");
+  let keyNum: number = 0;
+  let markerObj: MarkerValue;
+  let actMarkerTime: number = 0;
+
+  const selLayers: Layer[] = <Layer[]>actComp.selectedLayers;
+
   if (!isLayerSelected(selLayers)) {
-    return 0;
-  }
+    keyNum = <number>actComp.markerProperty.numKeys;
 
-	app.beginUndoGroup("Count Comment Marker");
-  let keyNum:number = 0;
-  let markerObj:MarkerValue;
-  let actMarkerTime:number;
-
-  for (let i=0; i<selLayers.length; i++){
-    keyNum = selLayers[i].property(ADBE_MARKER).numKeys;
-
-    if(keyNum == 0){
-        markerObj = new MarkerValue(keyNum+1);
-    }else{
-      actMarkerTime = selLayers[i].property(ADBE_MARKER).keyTime(selLayers[i].property(ADBE_MARKER).nearestKeyIndex(actComp.time));
-      if(actMarkerTime == actComp.time){
+    if (keyNum == 0) {
+      markerObj = new MarkerValue(keyNum + 1);
+    } else {
+      actMarkerTime = actComp.markerProperty.keyTime(actComp.markerProperty.nearestKeyIndex(actComp.time));
+      if (actMarkerTime == actComp.time) {
         markerObj = new MarkerValue(keyNum);
-      }else{
-        markerObj = new MarkerValue(keyNum+1);
+      } else {
+        markerObj = new MarkerValue(keyNum + 1);
       }
     }
 
-    selLayers[i].property(ADBE_MARKER).setValueAtTime(actComp.time, markerObj);
+    actComp.markerProperty.setValueAtTime(actComp.time, markerObj);
+    return 0;
   }
 
-	app.endUndoGroup();
+  for (let layer of selLayers) {
+    keyNum = layer.property(ADBE_MARKER).numKeys;
+
+    if (keyNum == 0) {
+      markerObj = new MarkerValue(keyNum + 1);
+    } else {
+      actMarkerTime = layer.property(ADBE_MARKER).keyTime(layer.property(ADBE_MARKER).nearestKeyIndex(actComp.time));
+      if (actMarkerTime == actComp.time) {
+        markerObj = new MarkerValue(keyNum);
+      } else {
+        markerObj = new MarkerValue(keyNum + 1);
+      }
+    }
+
+    layer.property(ADBE_MARKER).setValueAtTime(actComp.time, markerObj);
+  }
+
+  app.endUndoGroup();
 
 }).call(this);
